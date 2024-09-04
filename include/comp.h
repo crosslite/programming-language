16439f388cf599a2e277ade5d9cae5ae6447af54
crosslite <crosslite.public@gmail.com>
2024-09-04 15:47:18 +0700

    update

--- include/comp.h
+++ include/comp.h
@@ -1,6 +1,19 @@
 #ifndef HEADER_comp
 #define HEADER_comp
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define VERSION "0.0"
+
+static char *input_files_paths[32];
+static FILE *input_files[32];
+
 extern int comp_main(int argc, char **argv);
+
+extern int comp_init(int argc, char **argv);
+
+void void print_usage(void);
 
 #endif /* HEADER_comp */
--- src/comp.c
+++ src/comp.c
@@ -2,6 +2,34 @@
 
 int comp_main(int argc, char **argv)
 {
-
+        comp_init(argc, argv);
         return 0;
+}
+
+int comp_init(int argc, char **argv)
+{
+        --argc; ++argv;
+
+        for (int i = 0; i < argc; ++i) {
+                if (!strcmp(argv[i], "--help")) {
+                        print_usage();
+                        exit(0);
+                }
+
+                if (!strcmp(argv[i], "--version")) {
+                        printf("compiler %s\n", VERSION);
+                        exit(0);
+                }
+        }
+}
+
+void print_usage(void)
+{
+        printf(
+                "Usage: compiler [options] file...\n"
+                "Options:\n"
+                "--help    Display this information.\n"
+                "--version Display compiler version information.\n"
+                "-o <file> Place the output into <file>.\n"
+        );
 }
