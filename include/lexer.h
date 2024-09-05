9ed63fb2ab71e7644fedb9150dcce3733f45e394
crosslite <crosslite.public@gmail.com>
2024-09-05 11:20:03 +0700

    update

---
+++ include/lexer.h
@@ -0,0 +1,6 @@
+#ifndef HEADER_lexer
+#define HEADER_lexer
+
+extern int lexer_main(void);
+
+#endif /* HEADER_lexer */
---
+++ src/lexer.c
@@ -0,0 +1,7 @@
+#include "lexer.h"
+
+int lexer_main(void)
+{
+
+        return 0;
+}
